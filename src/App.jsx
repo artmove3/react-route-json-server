69c78d927d8f123272fbdfd43a32e954bebe4b33
Andrei Shilov <artmove3@gmail.com>
2025-07-12 09:08:23 +0700

    cleared unused stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,6 @@
-import { createElement, useState } from 'react';
-// import ReactLogo from './assets/react.svg?react';
-import reactLogo from './assets/react.svg';
 import './App.css';
 import { ShowDate } from './components/ShowDate';
 
-function App() {
-	const [count, setCount] = useState(0);
-
-	const logoImg = createElement('img', { src: reactLogo });
-	const logoContainer = createElement('div', { key: 'logoContainer' }, logoImg);
-	const title = createElement('h1', { key: 'title' }, 'Vite + React');
-	const button = createElement(
-		'button',
-		{
-			key: 'button',
-			onClick: () => setCount((count) => count + 1),
-		},
-		`count is ${count}`,
-	);
-
-	const p = createElement('p', { key: 'p' }, `Edit src/App.jsx and save to test HMR`);
-
-	const card = createElement('div', { className: 'card', key: 'card' }, [button, p]);
-	const container = createElement('div', null, [
-		logoContainer,
-		title,
-		card,
-		ShowDate(),
-	]);
-
-	return container;
-}
+function App() {}
 
 export default App;
--- src/components/ShowDate.jsx
+++
@@ -1,8 +0,0 @@
-import { createElement } from 'react';
-
-export function ShowDate() {
-	const date = new Date();
-	const current = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
-
-	return createElement('div', { key: 'date' }, current);
-}
